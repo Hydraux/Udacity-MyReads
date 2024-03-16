5e898debbc7d15d98f991ca256898000f83d2955
Hydraux <ryan.bezold@gmail.com>
2024-03-16 02:56:17 -0500

    feature: Sort books by shelf

--- starter/src/components/Bookshelf.js
+++ starter/src/components/Bookshelf.js
@@ -1,63 +1,12 @@
-import { useEffect, useState } from "react";
-import { getAll } from "../BooksAPI";
 import Book from "./Book";
 
-function Bookshelf() {
-  const [books, setBooks] = useState([]);
-
-  console.log(books);
-
-  /**
-   * Grabs all books from the API and sets the state of this component.
-   * We will not make the API call again if the component is already mounted.
-   * When the cleanup function is called, the mounted flag is toggled so we are ready to make the API call again.
-   **/
-  useEffect(() => {
-    let mounted = true;
-
-    if (mounted) {
-        getAll().then((res) => setBooks(res));
-    }
-
-    return () => {
-      mounted = false;
-    };
-  }, []);
-
+function Bookshelf({books, title}) {
   return (
     <div className="bookshelf">
-      <h2 className="bookshelf-title">Currently Reading</h2>
+      <h2 className="bookshelf-title">{title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
           {books.map((book)=><li key={book.id}><Book book={book} /></li>)}
-          <li>
-            <div className="book">
-              <div className="book-top">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 188,
-                    backgroundImage:
-                      'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")',
-                  }}
-                ></div>
-                <div className="book-shelf-changer">
-                  <select>
-                    <option value="none" disabled>
-                      Move to...
-                    </option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">Ender's Game</div>
-              <div className="book-authors">Orson Scott Card</div>
-            </div>
-          </li>
         </ol>
       </div>
     </div>

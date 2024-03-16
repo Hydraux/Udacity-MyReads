b094b08a2e19268d83bb5479602da866ba5e3ec0
Hydraux <ryan.bezold@gmail.com>
2024-03-16 02:34:20 -0500

    style: Fix formatting

--- starter/src/components/Book.js
+++ starter/src/components/Book.js
@@ -1,8 +1,8 @@
 import BookshelfChanger from "./BookshelfChanger";
 
 /**
- * 
- * @param {object} Book - An object created by fetching data from https://reactnd-books-api.udacity.com 
+ *
+ * @param {object} Book - An object created by fetching data from https://reactnd-books-api.udacity.com
  * @description Parses data from a book object and inserts it into a component
  * @returns {JSX.Element} Component containing the cover, title, and authors of a book
  */
@@ -15,18 +15,15 @@ function Book({ book }) {
           style={{
             width: 128,
             height: 193,
-            backgroundImage: 'url('+book.imageLinks.thumbnail+')',
+            backgroundImage: "url(" + book.imageLinks.thumbnail + ")",
           }}
         ></div>
         <BookshelfChanger />
       </div>
       <div className="book-title">{book.title}</div>
 
-      
       <div className="book-authors">
-        <span>
-            {book.authors.map((author)=>author)}
-        </span>
+        <span>{book.authors.map((author) => author)}</span>
       </div>
     </div>
   );
